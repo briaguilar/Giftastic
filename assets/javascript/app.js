827d0f1547a2196c1e68e745fb14882dfe55b6d3
Bri <breezy.aguilar@gmail.com>
2019-06-19 14:50:52 -0500

    gif/image sources for play/pause have been identified

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
         var person = $(this).attr("data-person");
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=qQSqkVtM3n2TAwKHxosoCWlxDiFocfpw&limit=15";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=qQSqkVtM3n2TAwKHxosoCWlxDiFocfpw&limit=9";
 
         // Performing AJAX GET request
         $.ajax({
@@ -28,17 +28,28 @@ $(document).ready(function() {
 
                         var p = $("<p>").text("Rating: " + rating);
 
-                        var personImage = $("<img>");
+                        var personImage = $("<img class='gif'>");
+                        var play = results[i].images.fixed_height.url;
+                        var still = results[i].images.original_still.url;
+                        var state = $(this).attr("src")
 
-                        personImage.attr("src", results[i].images.fixed_height.url);
+                        personImage.attr("src", play);
 
                         gifDiv.append(p);
-                        gifDiv.append(personImage);
+                        $(gifDiv).append(personImage);
                         
                         $("#gifs-appear-here").prepend(gifDiv);
-                        
-                    }
+
+                        $(".gif").on("click", function() {
+                            if (state === play) {
+                                $(this).attr("src", )
+                            }
+                        })
                 }
+                }
+                
+                
             })
     })
+    
 })
